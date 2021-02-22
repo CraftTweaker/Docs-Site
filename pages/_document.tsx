97dbff80e3c2652d81ac8b95cb08157ba74ab1e5
Jared <jaredlll08@gmail.com>
2021-02-23 01:06:26 +0200

    don't do anything in dev

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,15 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import React from "react";
+import { DOCS_DEV, SITE_DEV } from "../utils/Utils";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
 
   render() {
     return (
       <Html lang = {"en"}>
 
         <Head>
-          <script async defer data-website-id="50e143af-e1f3-4c99-90c9-eb644de304c8" src="https://analytics.blamejared.com/umami.js"/>
+          {!SITE_DEV && DOCS_DEV && <script async defer data-website-id="50e143af-e1f3-4c99-90c9-eb644de304c8" src="https://analytics.blamejared.com/umami.js"/>}
         </Head>
         <body>
         <Main/>
