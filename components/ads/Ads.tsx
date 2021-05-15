4bd9eca0d95a4550779a8b16bae7ea18e81ddeaf
Jared <jaredlll08@gmail.com>
2021-05-16 01:35:39 +0200

    More testing

--- components/ads/Ads.tsx
+++ components/ads/Ads.tsx
@@ -2,4 +2,4 @@ import dynamic from "next/dynamic";
 
 export const DesktopAd = dynamic(() => import('./DesktopAd'), { ssr: false })
 export const MobileAd = dynamic(() => import('./MobileAd'), { ssr: false })
-export const AD_REFRESH_RATE = 30 * 1000;
+export const AD_REFRESH_RATE = 300 * 1000;
