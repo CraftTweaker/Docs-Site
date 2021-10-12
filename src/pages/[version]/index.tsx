2d1f863b0a04e10c266ba6bd59de7bab835db8ac
Jared <jaredlll08@gmail.com>
2021-10-13 01:15:39 +0200

    Add SEO and names for buttons

--- src/pages/[version]/index.tsx
+++ src/pages/[version]/index.tsx
@@ -5,34 +5,61 @@ import { Version } from "util/Types";
 import Image from "next/image";
 import React, { ReactElement } from "react";
 import Link from "next/link";
+import { NextSeo } from "next-seo";
 
 
 export default function VersionIndex(props: { version: Version, versions: string[] }): ReactElement {
 
     return <Layout>
-        <div className = {`flex min-h-screen`}>
+        <NextSeo
+            title={`CraftTweaker Documentation`}
+            description={`Documentation for the CraftTweaker Minecraft mod, information on how to use the ZenScript language and a central wiki for mods that rely on it.`}
+            canonical={`https://docs.blamejared.com/`}
+            openGraph={{
+                type: `website`,
+                url: `https://docs.blamejared.com/`,
+                title: `CraftTweaker Documentation`,
+                description: `Documentation for the CraftTweaker Minecraft mod, information on how to use the ZenScript language and a central wiki for mods that rely on it.`,
+                images: [
+                    {
+                        url: `https://docs.blamejared.com/og_image.png`,
+                        width: 90,
+                        height: 92,
+                        alt: `CraftTweaker logo`
+                    }
+                ]
+            }}
+            additionalMetaTags={[{
+                property: "keywords",
+                content: `CraftTweaker,CraftTweaker docs,CraftTweaker documentation,CraftTweaker wiki,CraftTweaker mod`
+            }, {
+                property: "charset",
+                content: `utf-8`
+            }]}
+        />
+        <div className={`flex min-h-screen`}>
 
-            <div className = {`flex-grow`}>
+            <div className={`flex-grow`}>
 
-                <div className = {`container w-5/6 mx-auto flex flex-col gap-y-4`}>
-                    <div className = {`flex flex-col gap-y-2 mt-4`}>
-                        <h2 className = {`text-5xl text-center`}>
+                <div className={`container w-5/6 mx-auto flex flex-col gap-y-4`}>
+                    <div className={`flex flex-col gap-y-2 mt-4`}>
+                        <h2 className={`text-5xl text-center`}>
                             Select Version
                         </h2>
-                        <div className = {`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-4 gap-x-2`}>
+                        <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-4 gap-x-2`}>
                             {props.versions.map(version => {
-                                    return props.version.version !== version ? <Link href = {`/${version}`} key = {version}>
+                                    return props.version.version !== version ? <Link href={`/${version}`} key={version}>
 
-                                        <a key = {version} className = {`versionButton`}>
+                                        <a key={version} className={`versionButton`}>
 
-                                            <p className = {`versionText`}>
+                                            <p className={`versionText`}>
                                                 {version}
                                             </p>
 
                                         </a>
 
-                                    </Link> : <div className = {`versionButtonSelected`} key = {version}>
-                                        <p className = {`versionText`}>
+                                    </Link> : <div className={`versionButtonSelected`} key={version}>
+                                        <p className={`versionText`}>
                                             {version}
                                         </p>
                                     </div>;
@@ -41,14 +68,15 @@ export default function VersionIndex(props: { version: Version, versions: string
                         </div>
                     </div>
 
-                    <div className = {`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mb-64`}>
+                    <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mb-64`}>
                         {props.version.languages.map(lang => {
-                            return <Link key = {`${props.version.version}-${lang}`} href = {`/${props.version.version}/${lang}/index`}>
+                            return <Link key={`${props.version.version}-${lang}`} href={`/${props.version.version}/${lang}/index`}>
 
                                 <a>
-                                    <div className = {`group component selectable w-full flex flex-col text-center`}>
-                                        <Image src = {`https://blamejared.com/svg/flags/${lang}.svg`} alt = {lang} width = {564} height = {423} layout = {`responsive`} className = {`dark:brightness-[80%]`} priority = {true} loading = {`eager`}/>
-                                        <div className = {`flex flex-col text-xl gap-y-1 py-2 select-none font-semibold`}>
+                                    <div className={`group component selectable w-full flex flex-col text-center`}>
+                                        <Image src={`https://blamejared.com/svg/flags/${lang}.svg`} alt={lang} width={564} height={423}
+                                               layout={`responsive`} className={`dark:brightness-[80%]`} priority={true} loading={`eager`}/>
+                                        <div className={`flex flex-col text-xl gap-y-1 py-2 select-none font-semibold`}>
                                             <p>Version: {props.version.version}</p>
                                             <p>Language: {lang}</p>
                                         </div>
